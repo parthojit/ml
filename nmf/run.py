b0ad4d62ff683a6d24a3a9e5257b007c9a4c91c0
PJ <parthojit@yahoo.com>
2022-07-11 16:21:06 +0900

    init

---
+++ nmf/run.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+class NMFTasks(object):
+    def __init__(self) -> None:
+        pass
+
+    def get_ranks(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+
