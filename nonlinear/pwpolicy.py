75c51d27077b4ca1710683b44155d5bb7ad148f7
PJ <parthojit@yahoo.com>
2022-12-23 23:09:36 +0900

    add other policies into power plan

--- nonlinear/pwpolicy.py
+++ nonlinear/pwpolicy.py
@@ -1,18 +1,20 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from solar import solar_cell_current
 
 
-TIME_STEP = 0.1
+TIME_STEP = 0.01
 i = 20
-dq_gps = i*TIME_STEP
-time_left_gps = 0
+dq_gps:float = i*TIME_STEP # mAh - cost of charge for GPS transmission
+time_left_gps:float = 0
 # energy_cost_gps = 10
-SOLAR_CELL_CHARGE = 100 # couloumbs
-BATTERY_CHARGE = 50 # coulombs
-TIME_UNTIL_GPS_EVENT = TIME_STEP*200 # 20 seconds
+SOLAR_CELL_CHARGE = 0 # couloumbs
+BATTERY_CHARGE_TOTAL = 100 # coulombs
+BATTERY_CHARGE_THRESHOLD = 70
+SOLAR_CELL_CURRENT_THRESHOLD = 6
 
-total_charge = SOLAR_CELL_CHARGE + BATTERY_CHARGE
+total_charge:float = SOLAR_CELL_CHARGE + BATTERY_CHARGE_TOTAL # mAh
 energy_left = 0
 count = 0
 
@@ -21,41 +23,125 @@ event_strike_gps = []
 event_time_gps = []
 
 
-def pp_max_aggresive():
-    pass
+def pp_max_aggresive(total_charge:float,count:int,time_left_gps:float,dq_gps:float,event_time_gps:list,event_strike_gps:list):
+    TIME_UNTIL_GPS_EVENT = TIME_STEP*20 # 2 seconds
+    if time_left_gps-TIME_STEP<0:
+        total_charge = total_charge - dq_gps
+        time_left_gps = TIME_UNTIL_GPS_EVENT
+        event_strike_gps.append(1)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        print("********* GPS Transmission has occurred *********")
+        time.sleep(TIME_STEP)
 
-def pp_max_conservative():
-    pass
+    else:
+        time_left_gps = time_left_gps-TIME_STEP
+        event_strike_gps.append(0)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        time.sleep(TIME_STEP)
 
-def pp_mod_aggresive():
-    pass
+    print("GPS TIME LEFT UNTIL NEXT EVENT = %.2f" % time_left_gps)
+    print("(PP: Max Aggressive) TOTAL CHARGE LEFT = %.2f" % total_charge)
+    return total_charge,count+1,time_left_gps,event_time_gps,event_strike_gps
 
-def pp_mod_conservative():
-    pass
+def pp_mod_aggressive(total_charge:float,count:int,time_left_gps:float,dq_gps:float,event_time_gps:list,event_strike_gps:list):
+    TIME_UNTIL_GPS_EVENT = TIME_STEP*40 # 4 seconds
+    if time_left_gps-TIME_STEP<0:
+        total_charge = total_charge - dq_gps
+        time_left_gps = TIME_UNTIL_GPS_EVENT
+        event_strike_gps.append(1)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        print("********* GPS Transmission has occurred *********")
+        time.sleep(TIME_STEP)
+
+    else:
+        time_left_gps = time_left_gps-TIME_STEP
+        event_strike_gps.append(0)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        time.sleep(TIME_STEP)
+
+    print("GPS TIME LEFT UNTIL NEXT EVENT = %.2f" % time_left_gps)
+    print("(PP: Mod Aggressive) TOTAL CHARGE LEFT = %.2f" % total_charge)
+    return total_charge,count+1,time_left_gps,event_time_gps,event_strike_gps
+
+def pp_mod_conservative(total_charge:float,count:int,time_left_gps:float,dq_gps:float,event_time_gps:list,event_strike_gps:list):
+    TIME_UNTIL_GPS_EVENT = TIME_STEP*60 # 6 seconds
+    if time_left_gps-TIME_STEP<0:
+        total_charge = total_charge - dq_gps
+        time_left_gps = TIME_UNTIL_GPS_EVENT
+        event_strike_gps.append(1)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        print("********* GPS Transmission has occurred *********")
+        time.sleep(TIME_STEP)
+
+    else:
+        time_left_gps = time_left_gps-TIME_STEP
+        event_strike_gps.append(0)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        time.sleep(TIME_STEP)
+
+    print("GPS TIME LEFT UNTIL NEXT EVENT = %.2f" % time_left_gps)
+    print("(PP: Mod Conservative) TOTAL CHARGE LEFT = %.2f" % total_charge)
+    return total_charge,count+1,time_left_gps,event_time_gps,event_strike_gps
+
+def pp_max_conservative(total_charge:float,count:int,time_left_gps:float,dq_gps:float,event_time_gps:list,event_strike_gps:list):
+    TIME_UNTIL_GPS_EVENT = TIME_STEP*80 # 8 seconds
+    if time_left_gps-TIME_STEP<0:
+        total_charge = total_charge - dq_gps
+        time_left_gps = TIME_UNTIL_GPS_EVENT
+        event_strike_gps.append(1)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        print("********* GPS Transmission has occurred *********")
+        time.sleep(TIME_STEP)
+
+    else:
+        time_left_gps = time_left_gps-TIME_STEP
+        event_strike_gps.append(0)
+        event_time_gps.append(TIME_STEP*count)
+        event_counter_gps.append(time_left_gps)
+        time.sleep(TIME_STEP)
+
+    print("GPS TIME LEFT UNTIL NEXT EVENT = %.2f" % time_left_gps)
+    print("(PP: Max Conservative) TOTAL CHARGE LEFT = %.2f" % total_charge)
+    return total_charge,count+1,time_left_gps,event_time_gps,event_strike_gps
 
 start = time.time()
 
 while True:
     try:
-        count+=1
-        if time_left_gps-TIME_STEP<0:
-            total_charge = total_charge - dq_gps
-            time_left_gps = TIME_UNTIL_GPS_EVENT
-            event_strike_gps.append(1)
-            event_time_gps.append(TIME_STEP*count)
-            event_counter_gps.append(time_left_gps)
-            print("********* GPS Transmission has occurred *********")
-            time.sleep(TIME_STEP/10000)
+        if total_charge > BATTERY_CHARGE_THRESHOLD and solar_cell_current[count] > SOLAR_CELL_CURRENT_THRESHOLD:
+            total_charge,count,time_left_gps,event_time_gps,event_strike_gps = pp_max_aggresive(total_charge=total_charge,count=count,time_left_gps=time_left_gps,
+            dq_gps=dq_gps,event_time_gps=event_time_gps,event_strike_gps=event_strike_gps)
+        
+
+        elif total_charge > BATTERY_CHARGE_THRESHOLD and solar_cell_current[count] < SOLAR_CELL_CURRENT_THRESHOLD:
+            total_charge,count,time_left_gps,event_time_gps,event_strike_gps = pp_mod_aggressive(total_charge=total_charge,count=count,time_left_gps=time_left_gps,
+            dq_gps=dq_gps,event_time_gps=event_time_gps,event_strike_gps=event_strike_gps)
+
+
+        elif total_charge < BATTERY_CHARGE_THRESHOLD and solar_cell_current[count] > SOLAR_CELL_CURRENT_THRESHOLD:
+            total_charge,count,time_left_gps,event_time_gps,event_strike_gps = pp_mod_conservative(total_charge=total_charge,count=count,time_left_gps=time_left_gps,
+            dq_gps=dq_gps,event_time_gps=event_time_gps,event_strike_gps=event_strike_gps)
+
+
+        elif total_charge < BATTERY_CHARGE_THRESHOLD and solar_cell_current[count] < SOLAR_CELL_CURRENT_THRESHOLD:
+            total_charge,count,time_left_gps,event_time_gps,event_strike_gps = pp_max_conservative(total_charge=total_charge,count=count,time_left_gps=time_left_gps,
+            dq_gps=dq_gps,event_time_gps=event_time_gps,event_strike_gps=event_strike_gps)
 
         else:
-            time_left_gps = time_left_gps-TIME_STEP
-            event_strike_gps.append(0)
-            event_time_gps.append(TIME_STEP*count)
-            event_counter_gps.append(time_left_gps)
-            time.sleep(TIME_STEP/10000)
+            total_charge,count,time_left_gps,event_time_gps,event_strike_gps = pp_max_aggresive(total_charge=total_charge,count=count,time_left_gps=time_left_gps,
+            dq_gps=dq_gps,event_time_gps=event_time_gps,event_strike_gps=event_strike_gps)
 
-        print("GPS TIME LEFT UNTIL NEXT EVENT = %.2f" % time_left_gps)
-        print("TOTAL CHARGE LEFT = %.2f" % total_charge)
+
+
+        print("-- count : %2d" % count)
+        print("-- solar cell current : %2d" % solar_cell_current[count])
         
     except KeyboardInterrupt:
         print("--  Keyboard Interrupt!")
@@ -64,6 +150,6 @@ while True:
 plt.plot(event_time_gps,event_strike_gps)
 plt.xlabel("Event Time")
 plt.ylabel("Event Strike")
-plt.show()
+plt.savefig("./powerpolicy.png",dpi=300,bbox_inches="tight")
 
 
---
+++ nonlinear/solar.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+TIME_STEP = 0.1
+HOUR = 24
+MIN = 60
+# SEC = 60
+SEC = 1
+STEPS = 1/TIME_STEP
+
+SIZE = int(STEPS*SEC*MIN*HOUR)
+
+print(SIZE)
+
+solar_cell_current = np.zeros((SIZE))
+solar_cell_current[0:int(SIZE/HOUR)] = 1
+print(solar_cell_current)
+
+SET = 1
+for i in range(0,24):
+    solar_cell_current[int(SIZE/HOUR)*i:int(SIZE/HOUR)*(i+1)] = SET
+    
+    if i<11:
+        SET+=1
+    else:
+        SET-=1
+# plt.plot(solar_cell_current)
+# plt.show()
+
+
